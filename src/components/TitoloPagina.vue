b5268c049f2dfcab9c289bfc2ab7b3c7cdfc1222
AdrianBottini <adrianboh@gmail.com>
2022-11-24 12:48:22 +0100

    mod

---
+++ src/components/TitoloPagina.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="titolo">
+      Ciao a tutti dal componente
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitoloPagina',
+};
+</script>
+
+<style lang="scss" scoped>
+$title_color: red;
+
+div {
+ background-color: blue;
+  .titolo {
+    color: $title_color;
+  }
+}
+</style>
